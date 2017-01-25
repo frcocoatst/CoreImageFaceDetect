07ae191502fb00b9cc5f4728044e3b7e63319baf
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2017-01-25 14:02:47 +0100

    update

--- FaceDectect/MyView.swift
+++ FaceDectect/MyView.swift
@@ -5,6 +5,8 @@
 //  Created by Friedrich HAEUPL on 23.01.17.
 //  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
 //
+//  https://www.hackingwithswift.com/example-code/media/cidetectortypeface-how-to-detect-faces-in-a-uiimage
+//  https://www.appcoda.com/face-detection-core-image/
 
 import Cocoa
 
@@ -20,24 +22,28 @@ class MyView: NSView {
         var epos1: CGPoint = CGPoint()
         var epos2: CGPoint = CGPoint()
         
-        let fileURL = Bundle.main.url(forResource: "people", withExtension: "jpg")
+        // avoid optional unwrapping later :
+        // 1
+        guard let fileURL = Bundle.main.url(forResource: "people", withExtension: "jpg")
+        else
+        {
+            NSLog("ciImage doesn't exist")
+            return
+        }
         // 2
-        let ciImage = CIImage(contentsOf: fileURL!)
-        NSLog("ciImage extent \(ciImage!.extent)")
+        guard let ciImage = CIImage(contentsOf: fileURL)
+        else
+        {
+            NSLog("ciImage not loaded")
+            return
+        }
 
-        // 3
-        // https://www.hackingwithswift.com/example-code/media/cidetectortypeface-how-to-detect-faces-in-a-uiimage
-/*
-        // Adjusts saturation, brightness, and contrast values.
-        // To calculate saturation, this filter linearly interpolates between a grayscale image (saturation = 0.0) and the original image (saturation = 1.0). The filter supports extrapolation: For values large than 1.0, it increases saturation.
-        let filter = CIFilter(name: "CIGloom")
-        filter!.setValue(beginImage, forKey: kCIInputImageKey)
-        filter!.setValue(10.0, forKey: "inputRadius")
-        filter!.setValue(1.5,  forKey: "inputIntensity")
-*/
-        // 4 Render Image
-        // convert CIImage to NSImage
-        let rep: NSCIImageRep = NSCIImageRep(ciImage: ciImage!)
+        // 3    get size of the image
+        let ciImageSize = ciImage.extent.size
+        NSLog("ciImage extent \(ciImage.extent)")
+
+        // 4    convert CIImage to NSImage
+        let rep: NSCIImageRep = NSCIImageRep(ciImage: ciImage)
         let nsImage: NSImage = NSImage(size: rep.size)
         nsImage.addRepresentation(rep)
         
@@ -46,16 +52,34 @@ class MyView: NSView {
         // 2) Or stretch image to fill view
         nsImage.draw(in: self.bounds, from:NSZeroRect ,operation:.sourceOver, fraction:1)
         
-        let options = [CIDetectorAccuracy: CIDetectorAccuracyLow]
+        let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
         
         let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: options)!
         
-        let faces = faceDetector.features(in: ciImage!)
+        let faces = faceDetector.features(in: ciImage)
         
         for face in faces as! [CIFaceFeature] {
         //if let face = faces.first as? CIFaceFeature {
+            NSLog("---------")
             NSLog("Found face at \(face.bounds) of \(faces.count) Faces")
+
+            // 
+            var faceViewBounds = face.bounds
             
+            // Calculate the actual position and size of the rectangle in the image view
+            let viewSize = dirtyRect.size
+            //  if scales in both directions equally :
+            //  let scale = min(viewSize.width / (ciImageSize?.width)!,
+            //                viewSize.height / (ciImageSize?.height)!)
+            let scale_w = viewSize.width / (ciImageSize.width)
+            let scale_h = viewSize.height / (ciImageSize.height)
+            let offsetX = (viewSize.width - (ciImageSize.width) * scale_w) / 2.0
+            let offsetY = (viewSize.height - (ciImageSize.height) * scale_h) / 2.0
+            
+            faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale_w, y: scale_h))
+            faceViewBounds.origin.x += offsetX
+            faceViewBounds.origin.y += offsetY
+            NSLog("faceViewBounds is \(faceViewBounds)")
             
             if face.hasLeftEyePosition {
                 epos1 = face.leftEyePosition
@@ -79,32 +103,40 @@ class MyView: NSView {
             // draw rectangle https://www.appcoda.com/face-detection-core-image/
             //
             NSColor.red.set()
-            let path:NSBezierPath = NSBezierPath()
-            let rect = NSRect(x: (face.bounds.origin.x),
-                              y: (face.bounds.origin.y),
-                              width: (face.bounds.size.width),
-                              height: (face.bounds.size.height))
-            
-            path.appendRect(rect)
+            let path:NSBezierPath = NSBezierPath()            
+            path.appendRect(faceViewBounds)
 
-            let mrect = NSRect(x: mpos.x - 10 ,
+            var mrect = NSRect(x: mpos.x - 10 ,
                               y: mpos.y - 5 ,
                               width: 20,
                               height: 10)
             
+            mrect = mrect.applying(CGAffineTransform(scaleX: scale_w, y: scale_h))
+            mrect.origin.x += offsetX
+            mrect.origin.y += offsetY
+
+            
             path.appendRect(mrect)
 
-            let erect1 = NSRect(x: epos1.x - 10 ,
+            var erect1 = NSRect(x: epos1.x - 10 ,
                                y: epos1.y - 10 ,
                                width: 20,
                                height: 20)
             
+            erect1 = erect1.applying(CGAffineTransform(scaleX: scale_w, y: scale_h))
+            erect1.origin.x += offsetX
+            erect1.origin.y += offsetY
+            
             path.appendRect(erect1)
             
-            let erect2 = NSRect(x: epos2.x - 10 ,
+            var erect2 = NSRect(x: epos2.x - 10 ,
                                 y: epos2.y - 10 ,
                                 width: 20,
                                 height: 20)
+            
+            erect2 = erect2.applying(CGAffineTransform(scaleX: scale_w, y: scale_h))
+            erect2.origin.x += offsetX
+            erect2.origin.y += offsetY
             
             path.appendRect(erect2)
 
